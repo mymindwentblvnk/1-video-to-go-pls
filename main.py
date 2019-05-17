6498884e48ce57740fd9b84d1863a07a2d45b673
Michael Eichenseer <michael.eichenseer@codecentric.de>
2019-05-17 16:11:01 +0200

    Work with wrapper classes.

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
-from google.oauth2 import service_account
+from datetime import datetime
+import os
+
 from apiclient.http import MediaFileUpload
 from googleapiclient import discovery
+from google.oauth2 import service_account
+
 from pytube import YouTube
 
 
-# YouTube('https://youtu.be/9bZkp7q19f0').streams.filter(file_extension='mp4').first().download(filename='video')
+MIME_TYPE = 'video/mp4'
 
-def credentials_from_file():
-    SCOPES = ['https://www.googleapis.com/auth/drive']
-    SERVICE_ACCOUNT_FILE = 'service-account.json'
-    credentials = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE,
-                                                                        scopes=SCOPES)
-    return credentials
+
+class YouTubeClient(object):
+
+    def download(self, url):
+        video = YouTube(url)
+        file_name = datetime.now().strftime('%Y-%m-%d-%H-%M-%s')
+        video.streams\
+            .filter(mime_type=MIME_TYPE)\
+            .first()\
+            .download(filename=file_name)
+        return '{}.mp4'.format(file_name)
+
+
+class GoogleDriveClient(object):
+
+    scopes = ['https://www.googleapis.com/auth/drive']
+
+    def __init__(self, google_drive_folder_id, service_account_file='service-account.json'):
+        self.google_drive_folder_id = google_drive_folder_id
+        self.credentials = self._get_credentials(service_account_file)
+        self.service = discovery.build('drive', 'v3', credentials=self.credentials)
+
+    def _get_credentials(self, service_account_file):
+        return service_account.Credentials.from_service_account_file(service_account_file,
+                                                                     scopes=self.scopes)
+
+    def upload_file(self, file_path):
+        file_metadata = {
+            'name': file_path,
+            'parents': [self.google_drive_folder_id, ],
+        }
+        with open(file_path, 'r') as file_in:
+            media = MediaFileUpload(file_in.name, mimetype=MIME_TYPE, resumable=True)
+            request = self.service.files().create(body=file_metadata, media_body=media)
+            response = None
+            while response is None:
+                _, response = request.next_chunk()
+
+        os.remove(file_path)
 
 
 if __name__ == '__main__':
-    credentials = credentials_from_file()
-    service = discovery.build('drive', 'v3', credentials=credentials)
-
-    file_metadata = {
-        'name': 'video.mp4',
-        'parents': ['1mI969yzWh8PX0uK8M_SVKkxfNurLUusT']
-    }
-
-    # https://github.com/cfinch/Shocksolution_Examples/blob/master/GoogleCloudPlatform/driveAPIexample.py
-    with open('video.mp4', 'r') as tf:
-        media = MediaFileUpload(tf.name, mimetype='video/mp4')
-        cloudFile = service.files().create(body=file_metadata, media_body=media).execute()
+    google_folder_id = open('parent_id.txt').read().split()
+    file_name = YouTubeClient().download('https://www.youtube.com/watch?v=YNcBqW0CkpM')
+    GoogleDriveClient(google_folder_id).upload_file(file_name)
